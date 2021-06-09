ad97860a915d5602f0da34f27f979e5e5292fad7
ryo <if14germany1248@icloud.com>
2021-06-09 20:11:24 +0900

    ゲストの設定 #8

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # ゲストアカウントを取得する
   def self.guest
     find_or_create_by!(email: "guest@example.com") do |user|
       user.password = SecureRandom.urlsafe_base64
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root "homes#index"
-  devise_for :users
+
+  # ゲストユーザーを削除・パスワード再設定できないようにする
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+    passwords: "users/passwords"
+  }
 
   devise_scope :user do
     post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
