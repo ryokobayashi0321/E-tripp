59cbc0d92b2ddbc8c08ffb0aa2977c12bf1ec0fa
ryo <if14germany1248@icloud.com>
2021-06-09 16:59:34 +0900

    ゲストログイン機能を実装 #8

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root "homes#index"
   devise_for :users
+
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
 end
