6150d110c04375f18b0501f64bc356c769898ea4
ryo <if14germany1248@icloud.com>
2021-06-12 15:22:08 +0900

    ストロングパラメータ設定 #19

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,13 +1,27 @@
 module Users
   class RegistrationsController < Devise::RegistrationsController
     before_action :ensure_normal_user, only: %i[update destroy]
+    before_action :configure_sign_up_params, only: [:create]
+    before_action :configure_account_update_params, only: [:update]
 
     def ensure_normal_user
       redirect_to root_path, alert: "ゲストユーザーは更新・削除できません。" if resource.email == "guest@example.com"
     end
 
+    def create; end
+
     def update; end
 
     def destroy; end
+  end
+
+  private
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name, :avatar])
+  end
+
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:user_name, :avatar])
   end
 end
