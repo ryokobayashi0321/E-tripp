e3731557ce65c12ae23386a83c04451360f88663
ryo <if14germany1248@icloud.com>
2021-06-14 16:24:03 +0900

    プロフィール画像デフォルト設定 #18

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -44,4 +44,8 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
+
+  def default_url(*_args)
+    "/images/fallback/#{[version_name, 'default.png'].compact.join('_')}"
+  end
 end
