cc02f0cf1b5e830283506ea4641080e7550c5be6
ryo <if14germany1248@icloud.com>
2021-06-17 15:30:52 +0900

    crud処理実装 #27

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,5 +1,37 @@
 class PlansController < ApplicationController
+  def index
+    @plans = Plan.all
+  end
+
+  def show
+    @plan = Plan.find(params[:id])
+  end
+
   def new
     @plan = Plan.new
+  end
+
+  def create
+    Plan.create(plan_params)
+  end
+
+  def edit
+    @plan = Plan.find(params[:id])
+  end
+
+  def update
+    plan = Plan.find(params[:id])
+    plan.update(user_params)
+  end
+
+  def destroy
+    plan = Plan.find(params[:id])
+    plan.destroy
+  end
+
+  private
+
+  def plan_params
+    params.require(:plan).permit(:title, :day, :note, :user)
   end
 end
