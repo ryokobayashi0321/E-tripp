4a219c16bc07c2d842a609ca90aceab915f62d08
ryo <if14germany1248@icloud.com>
2021-06-19 17:31:35 +0900

    多対多 #29

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -2,6 +2,9 @@ class Spot < ApplicationRecord
   belongs_to :prefecture
   has_many :schedules, dependent: :destroy
 
+  # spot.scheduled_plansで spot を「指定した時間」のplanの一覧を取得できるようになる
+  has_many :scheduled_plans, through: :schedules, source: :plan
+
   validates :spot_name, presence: true
   validates :content, presence: true
   validates :photo, presence: true
