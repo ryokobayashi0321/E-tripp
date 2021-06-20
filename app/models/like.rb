6a1b58abcd255515145aa399be4eb0bce3b581e1
ryo <if14germany1248@icloud.com>
2021-06-20 13:35:31 +0900

    likeテーブルの設定 #34

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,9 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :spot
+
+  validates :user_id, uniqueness: {
+    scope: :spot_id,
+    message: "は同じスポットに2回以上いいねはできません"
+  }
 end
--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,6 +1,7 @@
 class Spot < ApplicationRecord
   belongs_to :prefecture
   has_many :schedules, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   # spot.scheduled_plansで spot を「指定した時間」のplanの一覧を取得できるようになる
   has_many :scheduled_plans, through: :schedules, source: :plan
