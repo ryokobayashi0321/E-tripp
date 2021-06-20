9fd7c965f916d1c3469ed153979cdbdc44a2212e
ryo <if14germany1248@icloud.com>
2021-06-20 14:53:22 +0900

    いいね機能を非同期に(js.erbを使用) #34

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,11 @@
 class LikesController < ApplicationController
   def create
-    current_user.likes.create!(spot_id: params[:spot_id])
-    redirect_back(fallback_location: root_path)
+    @spot = Spot.find(params[:spot_id])
+    current_user.likes.create!(spot_id: @spot.id)
   end
 
   def destroy
-    current_user.likes.find_by(spot_id: params[:spot_id]).destroy!
-    redirect_back(fallback_location: root_path)
+    @spot = Spot.find(params[:spot_id])
+    current_user.likes.find_by(spot_id: @spot.id).destroy!
   end
 end
