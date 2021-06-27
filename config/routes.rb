93750221d5aed7bca8896e269590afff7baa6a20
ryo <if14germany1248@icloud.com>
2021-06-27 15:55:45 +0900

    activeadmin導入　#46

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root "homes#index"
 
   # ゲストユーザーを削除・パスワード再設定できないようにする
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,2 @@
 # test
+AdminUser.create!(email: "admin@example.com", password: "password", password_confirmation: "password") if Rails.env.development?
