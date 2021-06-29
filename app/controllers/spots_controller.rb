0a85a4b5384fec7978247c7de60bb31c33003a10
ryo <if14germany1248@icloud.com>
2021-06-29 23:46:16 +0900

    spot修正　#50

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,49 +1,17 @@
 class SpotsController < ApplicationController
-  before_action :set_spot, only: %w[show edit update destroy]
   def index
-    # csvデータ表示
-    respond_to do |format|
-      format.html
-      format.csv do
-        send_data(@spots.generate_csv, filename: "spot_data.csv")
-      end
-    end
     @prefectures = Prefecture.all
     @q = Spot.ransack(params[:q])
     @spots = Spot.includes(:likes).order(id: :asc)
   end
 
   def show
+    @spot = Spot.find(params[:id])
     @comment = current_user.comments.new
     @comments = @spot.comments
   end
 
-  def new
-    @spot = Spot.new
-  end
-
-  def create
-    @spot = Spot.create!(spot_params)
-    redirect_to spot
-  end
-
-  def edit; end
-
-  def update
-    @spot = Spot.update!(spot_params)
-    redirect_to post
-  end
-
-  def destroy
-    @spot.destroy!
-    redirect_to root_path
-  end
-
   private
-
-  def set_spot
-    @spot = Spot.find(params[:id])
-  end
 
   def spot_params
     params.require(:spot).permit(:spot_name, :content, :photo)
