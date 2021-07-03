826ff36f07abe9419bc68efad42e88ba0fdbb0b8
ryo <if14germany1248@icloud.com>
2021-07-03 15:49:19 +0900

    修正　#54

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,9 +1,10 @@
 const { environment } = require('@rails/webpacker')
 
+// jQuery を Bootstrap 以外で使用
 const webpack = require('webpack')
 environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery'
+    $: 'jquery',
+    jQuery: 'jquery'
 }))
 
 module.exports = environment
