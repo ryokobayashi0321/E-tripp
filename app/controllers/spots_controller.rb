66ff307728689f07b50edf4260cde13156d26f51
ryo <if14germany1248@icloud.com>
2021-07-07 16:37:39 +0900

    コメント機能にページネーションを実装　#43

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,16 +1,17 @@
 class SpotsController < ApplicationController
-  PER_PAGE = 5
+  PER_PAGE_SPOT = 6
+  PER_PAGE_COMMENT = 3
 
   def index
     @prefectures = Prefecture.all
     @q = Spot.ransack(params[:q])
-    @spots = Spot.includes(:likes).order(id: :asc).page(params[:page]).per(PER_PAGE)
+    @spots = Spot.includes(:likes).order(id: :desc).page(params[:page]).per(PER_PAGE_SPOT)
   end
 
   def show
     @spot = Spot.find(params[:id])
     @comment = Comment.new
-    @comments = @spot.comments.order(created_at: :desc)
+    @comments = @spot.comments.order(created_at: :desc).page(params[:page]).per(PER_PAGE_COMMENT)
   end
 
   private
