aa1d76eb958115a7dacea4a78bf1b0a01e4effb9
ryo <if14germany1248@icloud.com>
2021-07-13 12:49:54 +0900

    修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
 # admin
-AdminUser.create!(email: "admin@example.com", password: "password", password_confirmation: "password") if Rails.env.development?
+AdminUser.create!(email: "admin@example.com", password: "password", password_confirmation: "password")
