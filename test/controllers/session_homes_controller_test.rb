916021a089f0bd80dd13069e46c2efe51c752cad
ryo <if14germany1248@icloud.com>
2021-07-19 14:51:07 +0900

    テストディレクトリ削除 #69

--- test/controllers/session_homes_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class SessionHomesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get session_homes_index_url
-    assert_response :success
-  end
-end
